829917c78ab0d6fdfd9a8d9a62433a35ce69a4b9
Your Name <lilin@wangxiaoer.com>
2019-08-01 14:26:25 +0800

    fix

--- request.go
+++ request.go
@@ -39,14 +39,16 @@ type (
 
 	// 统一收单交易退款接口
 	TradeRefundReq struct {
-		OutTradeNo   string `json:"out_trade_no"`   // 订单支付时传入的商户订单号,不能和 trade_no同时为空。
-		TradeNo      string `json:"trade_no"`       // 支付宝交易号，和商户订单号不能同时为空
-		RefundAmount string `json:"refund_amount"`  // 需要退款的金额，该金额不能大于订单金额,单位为元，支持两位小数]
-		OutRequestNo string `json:"out_request_no"` // 标识一次退款请求，同一笔交易多次退款需要保证唯一，如需部分退款，则此参数必传。
+		OutTradeNo     string `json:"out_trade_no"`    // 订单支付时传入的商户订单号,不能和 trade_no同时为空。
+		TradeNo        string `json:"trade_no"`        // 支付宝交易号，和商户订单号不能同时为空
+		RefundAmount   string `json:"refund_amount"`   // 需要退款的金额，该金额不能大于订单金额,单位为元，支持两位小数]
+		OutRequestNo   string `json:"out_request_no"`  // 标识一次退款请求，同一笔交易多次退款需要保证唯一，如需部分退款，则此参数必传。
+		RefundCurrency string `json:"refund_currency"` // 订单退款币种信息
+		RefundReason   string `json:"refund_reason"`   // 退款的原因说明
 	}
 
 	// alipay.trade.fastpay.refund.query(统一收单交易退款查询)
-	TradeRefundQuery struct {
+	TradeRefundQueryReq struct {
 		OutTradeNo   string `json:"out_trade_no"`   // 订单支付时传入的商户订单号,和支付宝交易号不能同时为空。 trade_no,out_trade_no如果同时存在优先取trade_no
 		TradeNo      string `json:"trade_no"`       // 支付宝交易号，和商户订单号不能同时为空
 		OutRequestNo string `json:"out_request_no"` // 请求退款接口时，传入的退款请求号，如果在退款请求时未传入，则该值为创建交易时的外部交易号

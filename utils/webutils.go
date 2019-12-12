bf742cc059d1c314ca558afcaede74d855a67dd6
Your Name <lilin@wangxiaoer.com>
2019-12-12 11:25:09 +0800

    fix

--- utils/webutils.go
+++ utils/webutils.go
@@ -30,58 +30,6 @@ func DoPost(postUrl string, m map[string]string) ([]byte, error) {
 }
 
 // --
-var quoteEscaper = strings.NewReplacer("\\", "\\\\", `"`, "\\\"")
-
-func DoPostUploadFile(postUrl string, m map[string]string, fileParams map[string]*FileItem) ([]byte, error) {
-	if fileParams == nil || len(fileParams) == 0 {
-		return DoPost(postUrl, m)
-	}
-
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-
-	for fieldName, file := range fileParams {
-		h := make(textproto.MIMEHeader)
-		h.Set("Content-Disposition",
-			fmt.Sprintf(`form-data; name="%s"; filename="%s"`,
-				quoteEscaper.Replace(fieldName), quoteEscaper.Replace(file.GetFileName())))
-		h.Set("Content-Type", file.GetMimeType())
-
-		part, err := writer.CreatePart(h)
-		if err != nil {
-			return nil, err
-		}
-
-		_, err = io.Copy(part, bytes.NewReader(file.GetContent()))
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	for key, val := range m {
-		_ = writer.WriteField(key, val)
-	}
-
-	if err := writer.Close(); err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequest("POST", postUrl, body)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Content-Type", writer.FormDataContentType()+" ;charset="+m[constant.Charset])
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	return ioutil.ReadAll(resp.Body)
-}
-
 func _buildQuery(params map[string]string) url.Values {
 	query := url.Values{}
 	for key, val := range params {
@@ -94,4 +42,71 @@ func _buildQuery(params map[string]string) url.Values {
 	}
 
 	return query
+}
+
+// --
+func DoPostUploadFile(postUrl string, m map[string]string, fileParams map[string]*FileItem) ([]byte, error) {
+	if fileParams == nil || len(fileParams) == 0 {
+		return DoPost(postUrl, m)
+	}
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	// 组装文本请求参数
+	for key, val := range m {
+		if err := writer.WriteField(key, val); err != nil {
+			return nil, err
+		}
+	}
+
+	// 组装文件请求参数
+	for fieldName, file := range fileParams {
+		if err := _getFileEntry(writer, fieldName, file); err != nil {
+			return nil, err
+		}
+	}
+
+	if err := writer.Close(); err != nil {
+		return nil, err
+	}
+
+	// 构建请求
+	req, err := http.NewRequest("POST", postUrl, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType()+" ;charset="+m[constant.Charset])
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return ioutil.ReadAll(resp.Body)
+}
+
+var quoteEscaper = strings.NewReplacer("\\", "\\\\", `"`, "\\\"")
+
+// 构建
+func _getFileEntry(writer *multipart.Writer, fieldName string, file *FileItem) error {
+	h := make(textproto.MIMEHeader)
+	h.Set("Content-Disposition",
+		fmt.Sprintf(`form-data; name="%s"; filename="%s"`,
+			quoteEscaper.Replace(fieldName), quoteEscaper.Replace(file.GetFileName())))
+	h.Set("Content-Type", file.GetMimeType())
+
+	part, err := writer.CreatePart(h)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(part, bytes.NewReader(file.GetContent()))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
